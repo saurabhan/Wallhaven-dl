b608ca57e9f1a17ebea51c2e8f8631f9f4049952
nagracks <iqbalsn11@yahoo.com>
2016-06-30 00:00:42 +0530

    Refactor
    For better readability

--- wallhaven-dl.py
+++ wallhaven-dl.py
@@ -8,13 +8,15 @@
 #                 Update - 29 June 2016                #
 ########################################################
 
-import os
 import getpass
-import bs4
+import os
 import re
+import time
+from textwrap import dedent
+
+import bs4
 import requests
 import tqdm
-import time
 
 os.makedirs('Wallhaven', exist_ok=True)
 
@@ -22,19 +24,21 @@ def login():
     print('NSFW images require login')
     username = input('Enter username: ')
     password = getpass.getpass('Enter password: ')
-    req = requests.post('https://alpha.wallhaven.cc/auth/login', data={'username':username, 'password':password})
+    req      = requests.post('https://alpha.wallhaven.cc/auth/login',
+                                                    data={'username':username,
+                                                          'password':password})
     return req.cookies
 
 def choice():
     print('''****************************************************************
                             Category Codes
 
-    all - Every wallpaper.
+    all     - Every wallpaper.
     general - For 'general' wallpapers only.
-    anime - For 'Anime' Wallpapers only.
-    people - For 'people' wallapapers only.
-    ga - For 'General' and 'Anime' wallapapers only.
-    gp - For 'General' and 'People' wallpapers only.
+    anime   - For 'Anime' Wallpapers only.
+    people  - For 'people' wallapapers only.
+    ga      - For 'General' and 'Anime' wallapapers only.
+    gp      - For 'General' and 'People' wallpapers only.
     ****************************************************************
     ''')
     ccode = input('Enter Category: ')
@@ -61,17 +65,23 @@ def choice():
     ****************************************************************
                             Purity Codes
 
-    sfw - For 'Safe For Work'
+    sfw     - For 'Safe For Work'
     sketchy - For 'Sketchy'
-    nsfw - For 'Not Safe For Work'
-    ws - For 'SFW' and 'Sketchy'
-    wn - for 'SFW' and 'NSFW'
-    sn - For 'Sketchy' and 'NSFW'
-    all - For 'SFW', 'Sketchy' and 'NSFW'
+    nsfw    - For 'Not Safe For Work'
+    ws      - For 'SFW' and 'Sketchy'
+    wn      - for 'SFW' and 'NSFW'
+    sn      - For 'Sketchy' and 'NSFW'
+    all     - For 'SFW', 'Sketchy' and 'NSFW'
     ****************************************************************
     ''')
     pcode = input('Enter Purity: ')
-    ptags = {'sfw':'100', 'sketchy':'010', 'nsfw':'001', 'ws':'110', 'wn':'101', 'sn':'011', 'all':'111'}
+    ptags = {'sfw'     : '100',
+             'sketchy' : '010',
+             'nsfw'    : '001',
+             'ws'      : '110',
+             'wn'      : '101',
+             'sn'      : '011',
+             'all'     : '111'}
     ptag = ptags[pcode]
 
     if pcode in ['nsfw', 'wn', 'sn', 'all']:
@@ -80,7 +90,7 @@ def choice():
         cookies = dict()
 
     CATURL = 'https://alpha.wallhaven.cc/search?categories=' + \
-        ctag + '&purity=' + ptag + '&page='
+                                            ctag + '&purity=' + ptag + '&page='
     return (CATURL, cookies)
 
 
@@ -105,23 +115,26 @@ def main():
     pgid = int(input('How Many pages you want to Download: '))
     print('Number of Wallpapers to Download: ' + str(24 * pgid))
     for i in range(1, pgid + 1):
-        url = BASEURL + str(i)
+        url    = BASEURL + str(i)
         urlreq = requests.get(url, cookies=cookies)
-        soup = bs4.BeautifulSoup(urlreq.text, 'lxml')
+        soup   = bs4.BeautifulSoup(urlreq.text, 'lxml')
         soupid = soup.findAll('a', {'class': 'preview'})
-        res = re.compile(r'\d+')
-        imgid = res.findall(str(soupid))
+        res    = re.compile(r'\d+')
+        imgid  = res.findall(str(soupid))
         imgext = ['jpg', 'png', 'bmp']
         for i in range(len(imgid)):
-            url = 'http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-%s.' % imgid[
-                i]
+            url = 'http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven'\
+                                                            '-%s.' % imgid[ i]
             for ext in imgext:
-                iurl = url + ext
+                iurl   = url + ext
                 imgreq = requests.get(iurl, cookies=cookies)
                 if imgreq.status_code == 200:
                     print('Downloading: ' + iurl)
-                    with open(os.path.join('Wallhaven', os.path.basename(iurl)), 'ab') as imageFile:
-                        for chunk in tqdm.tqdm(imgreq.iter_content(1024), total=(int(imgreq.headers['content-length']) / 1024), unit='KB'):
+                    with open(os.path.join('Wallhaven',\
+                                   os.path.basename(iurl)), 'ab') as imageFile:
+                        for chunk in tqdm.tqdm(imgreq.iter_content(1024),\
+                                total=(int(imgreq.headers['content-length'])\
+                                                           / 1024), unit='KB'):
                             time.sleep(0.01)
                             imageFile.write(chunk)
                 break
